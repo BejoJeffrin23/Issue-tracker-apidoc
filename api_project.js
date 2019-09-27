61f432e42f71387407dbd407940ee8916ecfe333
BejoJeffrin23 <bejojeffrin23@gmail.com>
2019-09-27 10:25:11 +0530

    api-doc

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Issue tracker application",
+  "version": "0.0.1",
+  "description": "Documentation for the issue tracker application",
+  "title": "Issue tracker APIs",
+  "url": "http://localhost:3000/api/v1/users",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-06T07:09:01.272Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
